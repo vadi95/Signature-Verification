41be2bbf665e894dc7596c636be9ae4371b1db37
Vadiraja K <vadi.fedx@gmail.com>
2019-05-02 11:41:47 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,23 +1,19 @@
-from pylab import *
+import cv2
+import imagehash
 import numpy as np
 from os import listdir
-from sklearn.svm import LinearSVC
-import cv2
+from pylab import *
 from PIL import Image
-from sklearn import svm
-import imagehash
 from scipy.cluster.vq import *
+from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
-from sklearn import tree
-from sklearn import linear_model
+from sklearn import tree, svm, linear_model
 
+genuine_images_path = "data/genuine"
+forged_images_path = "data/forged"
 
-
-genuine_image_filenames = listdir("data/genuine")
-forged_image_filenames = listdir("data/forged")
-
-genuine_image_paths = "data/genuine"
-forged_image_paths = "data/forged"
+genuine_image_filenames = listdir(genuine_images_path)
+forged_image_filenames = listdir(forged_images_path)
 
 genuine_image_features = [[] for x in range(12)]
 forged_image_features = [[] for x in range(12)]
@@ -31,8 +27,16 @@ for name in forged_image_filenames:
     forged_image_features[signature_id - 1].append({"name": name})
 
 
-def preprocess_image(path, display=False):
-    raw_image = cv2.imread(path)
+def preprocess_image(image_path, display=False):
+    """
+    1) Convert image to gray scale
+    2) Threshold image
+
+    :param image_path: image path
+    :param display: flag - if true display images
+    :return: preprocessed image
+    """
+    raw_image = cv2.imread(image_path)
     bw_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
     bw_image = 255 - bw_image
 
@@ -49,14 +53,14 @@ def preprocess_image(path, display=False):
     return threshold_image
 
 
-def get_contour_features(im, display=False):
-    '''
-    :param im: input preprocessed image
+def get_contour_features(preprocessed_image, display=False):
+    """
+    :param preprocessed_image: preprocessed image
     :param display: flag - if true display images
-    :return:aspect ratio of bounding rectangle, area of : bounding rectangle, contours and convex hull
-    '''
+    :return: aspect ratio of bounding rectangle, area of bounding rectangle, contours and convex hull
+    """
 
-    rect = cv2.minAreaRect(cv2.findNonZero(im))
+    rect = cv2.minAreaRect(cv2.findNonZero(preprocessed_image))
     box = cv2.boxPoints(rect)
     box = np.int0(box)
 
@@ -67,21 +71,21 @@ def get_contour_features(im, display=False):
     bounding_rect_area = w * h
 
     if display:
-        image1 = cv2.drawContours(im.copy(), [box], 0, (120, 120, 120), 2)
+        image1 = cv2.drawContours(preprocessed_image.copy(), [box], 0, (120, 120, 120), 2)
         cv2.imshow("a", cv2.resize(image1, (0, 0), fx=2.5, fy=2.5))
         cv2.waitKey()
 
-    hull = cv2.convexHull(cv2.findNonZero(im))
+    hull = cv2.convexHull(cv2.findNonZero(preprocessed_image))
 
     if display:
-        convex_hull_image = cv2.drawContours(im.copy(), [hull], 0, (120, 120, 120), 2)
+        convex_hull_image = cv2.drawContours(preprocessed_image.copy(), [hull], 0, (120, 120, 120), 2)
         cv2.imshow("a", cv2.resize(convex_hull_image, (0, 0), fx=2.5, fy=2.5))
         cv2.waitKey()
 
-    im2, contours, hierarchy = cv2.findContours(im.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, hierarchy = cv2.findContours(preprocessed_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     if display:
-        contour_image = cv2.drawContours(im.copy(), contours, -1, (120, 120, 120), 3)
+        contour_image = cv2.drawContours(preprocessed_image.copy(), contours, -1, (120, 120, 120), 3)
         cv2.imshow("a", cv2.resize(contour_image, (0, 0), fx=2.5, fy=2.5))
         cv2.waitKey()
 
@@ -92,30 +96,34 @@ def get_contour_features(im, display=False):
 
     return aspect_ratio, bounding_rect_area, hull_area, contour_area
 
+
 des_list = []
 
-def sift(im, path, display=False):
-    raw_image = cv2.imread(path)
+
+def sift(preprocessed_image, image_path, display=False):
+    raw_image = cv2.imread(image_path)
     sift = cv2.xfeatures2d.SIFT_create()
-    kp, des = sift.detectAndCompute(im, None)
+    kp, des = sift.detectAndCompute(preprocessed_image, None)
 
     if display:
-        cv2.drawKeypoints(im, kp, raw_image)
+        cv2.drawKeypoints(preprocessed_image, kp, raw_image)
         cv2.imshow('sift_keypoints.jpg', cv2.resize(raw_image, (0, 0), fx=3, fy=3))
         cv2.waitKey()
 
-    return (path, des)
+    return (image_path, des)
 
 
-cor = 0
-wrong = 0
+true_positive = 0
+true_negative = 0
+false_positive = 0
+false_negative = 0
 
 im_contour_features = []
 
 for i in range(12):
     des_list = []
     for im in genuine_image_features[i]:
-        image_path = genuine_image_paths + "/" + im['name']
+        image_path = genuine_images_path + "/" + im['name']
         preprocessed_image = preprocess_image(image_path)
         hash = imagehash.phash(Image.open(image_path))
 
@@ -128,13 +136,13 @@ for i in range(12):
         im['hull_area/bounding_area'] = convex_hull_area / bounding_rect_area
         im['contour_area/bounding_area'] = contours_area / bounding_rect_area
 
-        im_contour_features.append([hash, aspect_ratio, convex_hull_area / bounding_rect_area, contours_area / bounding_rect_area])
+        im_contour_features.append(
+            [hash, aspect_ratio, convex_hull_area / bounding_rect_area, contours_area / bounding_rect_area])
 
         des_list.append(sift(preprocessed_image, image_path))
 
-
     for im in forged_image_features[i]:
-        image_path = forged_image_paths + "/" + im['name']
+        image_path = forged_images_path + "/" + im['name']
         preprocessed_image = preprocess_image(image_path)
         hash = imagehash.phash(Image.open(image_path))
 
@@ -147,7 +155,8 @@ for i in range(12):
         im['hull_area/bounding_area'] = convex_hull_area / bounding_rect_area
         im['contour_area/bounding_area'] = contours_area / bounding_rect_area
 
-        im_contour_features.append([hash, aspect_ratio, convex_hull_area / bounding_rect_area, contours_area / bounding_rect_area])
+        im_contour_features.append(
+            [hash, aspect_ratio, convex_hull_area / bounding_rect_area, contours_area / bounding_rect_area])
 
         des_list.append(sift(preprocessed_image, image_path))
 
@@ -158,17 +167,17 @@ for i in range(12):
     voc, variance = kmeans(descriptors, k, 1)
 
     # Calculate the histogram of features
-    im_features = np.zeros((len(genuine_image_features[i]) + len(forged_image_features[i]), k+4), "float32")
+    im_features = np.zeros((len(genuine_image_features[i]) + len(forged_image_features[i]), k + 4), "float32")
     for i in range(len(genuine_image_features[i]) + len(forged_image_features[i])):
         words, distance = vq(des_list[i][1], voc)
         for w in words:
             im_features[i][w] += 1
 
         for j in range(4):
-            im_features[i][k+j] = im_contour_features[i][j]
+            im_features[i][k + j] = im_contour_features[i][j]
 
-    #nbr_occurences = np.sum((im_features > 0) * 1, axis=0)
-    #idf = np.array(np.log((1.0 * len(image_paths) + 1) / (1.0 * nbr_occurences + 1)), 'float32')
+    # nbr_occurences = np.sum((im_features > 0) * 1, axis=0)
+    # idf = np.array(np.log((1.0 * len(image_paths) + 1) / (1.0 * nbr_occurences + 1)), 'float32')
 
     # Scaling the words
     stdSlr = StandardScaler().fit(im_features)
@@ -178,32 +187,38 @@ for i in range(12):
 
     train_forged_features, test_forged_features = im_features[5:8], im_features[8:10]
 
-    #clf = linear_model.LogisticRegression(C=1e5)
+    # clf = linear_model.LogisticRegression(C=1e5)
 
     clf = LinearSVC()
-    #clf = tree.DecisionTreeClassifier()
-    #clf = tree.DecisionTreeRegressor()
-    #clf = svm.SVC()
-    clf.fit(np.concatenate((train_forged_features,train_genuine_features)), np.array([1 for x in range(len(train_forged_features))] + [2 for x in range(len(train_genuine_features))]))
+    # clf = tree.DecisionTreeClassifier()
+    # clf = tree.DecisionTreeRegressor()
+    # clf = svm.SVC()
 
+    clf.fit(np.concatenate((train_forged_features, train_genuine_features)),
+            np.array([1 for x in range(len(train_forged_features))] + [2 for x in range(len(train_genuine_features))]))
 
-
-    #print("2" + str(clf.predict(test_genuine_features)))
     genuine_res = clf.predict(test_genuine_features)
 
     for res in genuine_res:
         if int(res) == 2:
-            cor += 1
+            true_positive += 1
         else:
-            wrong += 1
+            false_negative += 1
 
-    #print("1" + str(clf.predict(test_forged_features)))
     forged_res = clf.predict(test_forged_features)
 
     for res in forged_res:
         if int(res) == 1:
-            cor += 1
+            true_negative += 1
         else:
-            wrong += 1
+            false_positive += 1
 
-print(float(cor)/(cor+wrong))
+accuracy = float(true_positive + true_negative) / (true_positive + true_negative + false_negative + false_positive)
+precision = float(true_positive) / (true_positive + false_positive)
+recall = float(true_positive) / (true_positive + false_negative)
+f1_score = float(2 * precision * recall) / (precision + recall)
+
+print("Accuracy: ", round(accuracy, 2))
+print("Precision: ", round(precision, 2))
+print("Recall: ", round(recall, 2))
+print("F1 score: ", round(f1_score, 2))
